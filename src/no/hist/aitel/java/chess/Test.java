3a79b9c075e1f31cb53825d3ad12d222b8147ba2
martin <martin@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-11 09:27:50 +0000

    Test
    git-svn-id: file:///home/martin/svn/repositories/chess@6 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/java/chess/Test.java
+++ src/no/hist/aitel/java/chess/Test.java
@@ -18,6 +18,7 @@ public class Test {
         // TODO code application logic here
         System.out.println("Test");
         System.out.println("hei!");
+        System.out.println("sdf");
     }
 
 }
