f3ee074e3698f5fc215023f03ccf19e579f58e33
martin <martin@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-11 09:16:35 +0000

    Initial import
    git-svn-id: file:///home/martin/svn/repositories/chess@1 a023ea85-94b2-48ad-8667-9024dedc06d2

---
+++ src/chess/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Main.java
+ * 
+ */
+
+package chess;
+
+/**
+ *
+ * @author martin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
