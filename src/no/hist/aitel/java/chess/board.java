242cf1c5a5fe41893f0044f5301a2fdcaed5f161
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-11 11:20:59 +0000

    test
    git-svn-id: file:///home/martin/svn/repositories/chess@11 a023ea85-94b2-48ad-8667-9024dedc06d2

---
+++ src/no/hist/aitel/java/chess/board.java
@@ -0,0 +1,58 @@
+
+
+package no.hist.aitel.java.chess;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Vegard
+ */
+public class board extends java.awt.Canvas  {
+    Color dunkel = new Color (0x999999);
+	Color hell = new Color (0xFFFFCC);
+
+    public board(java.applet.Applet ref) {
+        super();
+    }
+    
+    public void newgame () {
+        return;
+    }
+
+    public void paintBoard(Graphics g) {
+        g = getGraphics();
+        g.fillRect (3, 5, 40, 40);
+        int x = 1;
+        int y = 1;
+        boolean inverse = false;
+        for (int i = 1; i <= 64; i++) {
+            if (!inverse) {
+                if (i % 2 == 0) {
+                    g.setColor(dunkel);
+
+                } else {
+                    g.setColor(hell);
+                }
+            } else {
+                if (i % 2 == 0) {
+                    g.setColor(hell);
+                } else {
+                    g.setColor(dunkel);
+                }
+            }
+
+            if (i % 8 == 0) {
+                inverse = !inverse;                
+            }
+        }
+        g.fillRect (x, y, 40, 40);
+        x++;
+        y++;
+    }
+
+    public void run() {
+        
+    }
+}
