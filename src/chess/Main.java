f8cd2fa71a5a023689fa5a6c7410a1ea3fa36489
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-11 09:20:42 +0000

    test
    git-svn-id: file:///home/martin/svn/repositories/chess@3 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Test");
+        System.out.println("hei!");
     }
 
 }
