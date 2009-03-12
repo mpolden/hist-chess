2e1972de16d5269d83eac19e9511fe2629fb0a68
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-12 11:55:53 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@47 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/java/chess/gui/testDrawBoard.java
+++ src/no/hist/aitel/java/chess/gui/testDrawBoard.java
@@ -1,41 +1,24 @@
 package no.hist.aitel.java.chess.gui;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
 
 /**
  *
  * @author Vegard
  */
 
-public class testDrawBoard extends JFrame  {
-    JLayeredPane layeredPane;
-    drawBoard chessBoard;
-    JButton newGame = new JButton("New game");    
-    
+public class testDrawBoard {
+    JFrame frame = new JFrame("Chess Board");
+    drawBoard chesspanel = new drawBoard();
 
-    public testDrawBoard(String title) {
-        super(title);
-        Dimension boardSize = new Dimension(600, 600);
-        layeredPane = new JLayeredPane();
-        getContentPane().add(layeredPane);
-        layeredPane.setPreferredSize(boardSize);
-        chessBoard = new drawBoard();
-        layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-        chessBoard.setLayout(new GridLayout(8, 8));
-        chessBoard.setPreferredSize( boardSize );
-        chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);                     
-    }   
-    
-    public static void main(String[] args) {
-        JFrame frame = new testDrawBoard("Chess");        
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setResizable(true);
-        frame.setLocationRelativeTo(null);
+    public testDrawBoard() {
+        frame.getContentPane().add(chesspanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
         frame.setVisible(true);
+    }
+        
+    public static void main(String[] args) {
+        new testDrawBoard();
     }
 }
