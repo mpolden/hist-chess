2d0b3ef800fcdf38ccdc9835afdd3f06310aa33e
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-15 21:16:54 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@54 a023ea85-94b2-48ad-8667-9024dedc06d2

---
+++ src/no/hist/aitel/java/chess/gui/Images.java
@@ -0,0 +1,38 @@
+
+
+package no.hist.aitel.java.chess.gui;
+
+import javax.swing.ImageIcon;
+import no.hist.aitel.java.chess.pieces.Piece;
+
+/**
+ *
+ * @author Vegard
+ */
+public class Images {
+    final private int size = 64;
+    private Piece[] pieces = new Piece[size];
+    private ImageIcon[] pieceIcons = new ImageIcon[12];
+
+    public Images() {
+        pieceIcons[0] = new ImageIcon("images/pawnw.gif", "white pawn");
+        pieceIcons[1] = new ImageIcon("images/knightw.gif", "white knight");
+        pieceIcons[2] = new ImageIcon("images/bishopw.gif", "white bishop");
+        pieceIcons[3] = new ImageIcon("images/rookw.gif", "white rook");
+        pieceIcons[4] = new ImageIcon("images/queenw.gif", "white queen");
+        pieceIcons[5] = new ImageIcon("images/kingw.gif", "white king");
+        pieceIcons[6] = new ImageIcon("images/pawnb.gif", "black pawn");
+        pieceIcons[7] = new ImageIcon("images/knightb.gif", "black knight");
+        pieceIcons[8] = new ImageIcon("images/bishopb.gif", "black bishop");
+        pieceIcons[9] = new ImageIcon("images/rookb.gif", "black rook");
+        pieceIcons[10] = new ImageIcon("images/queenb.gif", "black queen");
+        pieceIcons[11] = new ImageIcon("images/kingb.gif", "black king");
+    }
+
+    public Piece[] getPieces() {
+        return pieces;
+    }
+    public ImageIcon getPieceIcon(int i) {
+        return pieceIcons[i];
+    }
+}
