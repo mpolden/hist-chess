8fd9e4cde4c48722582210ead42a154cc755a3fe
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-17 13:34:03 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@59 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/java/chess/gui/drawBoard.java
+++ src/no/hist/aitel/java/chess/gui/drawBoard.java
@@ -1,13 +1,7 @@
 package no.hist.aitel.java.chess.gui;
-import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 
@@ -16,10 +10,10 @@ import javax.swing.JPanel;
  * @author Vegard
  */
 public class drawBoard extends JPanel {
-    private BufferedImage bi;
-    private BufferedImage newBi;
-    static final int width = 80;
-    static final int height = 80;
+    private BufferedImage pawnb;
+    private BufferedImage pawnw;
+    static final int width = 117;
+    static final int height = 117;
     static final Color dunkel = new Color (0x999999);
     static final Color hell = new Color (0xFFFFCC);
     
@@ -55,44 +49,7 @@ public class drawBoard extends JPanel {
                 y+=1;
             }
         }
-        try {
-            URL imageSrc = new URL("http://www.chessvariants.com/d.pieces/pawnb.gif");
-            bi = ImageIO.read(imageSrc);
-            newBi = resize(bi, 80, 80);
-            
-        }
-
-        catch (IOException e) {
-            System.out.println("Image could not be read");
-        }
-
-       
-        for(int i=0; i<8; i++) {
-            g.drawImage(bi, 80*i, 130, this);
-            //g.drawImage(newBi, 80*i, 400, this);
-        }
-        
-    }
-    private static BufferedImage resize(BufferedImage image, int width, int height) {
-        int type = image.getType() == 0? BufferedImage.TYPE_INT_ARGB : image.getType();
-        BufferedImage resizedImage = new BufferedImage(width, height, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.setComposite(AlphaComposite.Src);
-
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-
-        g.setRenderingHint(RenderingHints.KEY_RENDERING,
-        RenderingHints.VALUE_RENDER_QUALITY);
-
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-        RenderingHints.VALUE_ANTIALIAS_ON);
-
-        g.drawImage(image, 0, 0, width, height, null);
-        g.dispose();
-        return resizedImage;
-    }
-    
+    }  
 }
 
 
