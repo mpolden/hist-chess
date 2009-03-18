dc63915dccf798672859f41b4c7d2bba4a4a6e7a
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-18 13:05:00 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@79 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/java/chess/gui/drawBoard.java
+++ src/no/hist/aitel/java/chess/gui/drawBoard.java
@@ -1,5 +1,6 @@
 package no.hist.aitel.java.chess.gui;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
@@ -12,10 +13,14 @@ import javax.swing.JPanel;
 public class drawBoard extends JPanel {
     private BufferedImage pawnb;
     private BufferedImage pawnw;
-    static final int width = 117;
-    static final int height = 117;
+    private static final int width = 117;
+    private static final int height = 117;
     static final Color dunkel = new Color (0x999999);
     static final Color hell = new Color (0xFFFFCC);
+    static final Color black = new Color (0x000000);
+    private Font arial = new Font("Arial", Font.PLAIN, 18);
+
+
     
     
     public drawBoard() {        
@@ -49,8 +54,12 @@ public class drawBoard extends JPanel {
                 y+=1;
             }
         }
-        for (int i = 0; i<8; i++) {
-            g.drawString("1", i*width, (y+1)*height);
+        for (int i = 1; i<=8; i++) {
+            String number ="";
+            number = number.valueOf(i);
+            g.setFont(arial);
+            g.setColor(black);
+            g.drawString(number, (i-1)*width, (y*height)-4);
         }
     }  
 }
