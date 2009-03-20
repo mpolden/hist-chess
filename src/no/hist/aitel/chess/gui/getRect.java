8e2bfaff11f7185d1ed235d37a75dd38f6ec987d
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-20 13:44:06 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@127 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/chess/gui/getRect.java
+++ src/no/hist/aitel/chess/gui/getRect.java
@@ -51,21 +51,65 @@ public class getRect {
     }
 
     public int getRectCoordX(int rectNumber) {
-        
-
+        if(rectNumber == 0 || rectNumber == 8 || rectNumber == 16 || rectNumber == 24 || rectNumber == 32 || rectNumber == 40 || rectNumber == 48 || rectNumber == 56) {
+            rectCoordX = (width*0)-xIn;
+        }
+        else if(rectNumber == 1 || rectNumber == 9 || rectNumber == 17 || rectNumber == 25 || rectNumber == 33 || rectNumber == 41 || rectNumber == 49 || rectNumber == 57) {
+            rectCoordX = (width*1)-xIn;
+        }
+        else if(rectNumber == 2 || rectNumber == 10 || rectNumber == 18 || rectNumber == 26 || rectNumber == 34 || rectNumber == 42 || rectNumber == 50 || rectNumber == 58) {
+            rectCoordX = (width*2)-xIn;
+        }
+        else if(rectNumber == 3 || rectNumber == 11 || rectNumber == 19 || rectNumber == 27 || rectNumber == 35 || rectNumber == 43 || rectNumber == 51 || rectNumber == 59) {
+            rectCoordX = (width*3)-xIn;
+        }
+        else if(rectNumber == 4 || rectNumber == 12 || rectNumber == 20 || rectNumber == 28 || rectNumber == 36 || rectNumber == 44 || rectNumber == 52 || rectNumber == 60) {
+            rectCoordX = (width*4)-xIn;
+        }
+        else if(rectNumber == 5 || rectNumber == 13 || rectNumber == 21 || rectNumber == 29 || rectNumber == 37 || rectNumber == 45 || rectNumber == 53 || rectNumber == 61) {
+            rectCoordX = (width*5)-xIn;
+        }
+        else if(rectNumber == 6 || rectNumber == 14 || rectNumber == 22 || rectNumber == 30 || rectNumber == 38 || rectNumber == 46 || rectNumber == 54 || rectNumber == 62) {
+            rectCoordX = (width*6)-xIn;
+        }
+        else if(rectNumber == 7 || rectNumber == 15 || rectNumber == 23 || rectNumber == 31 || rectNumber == 39 || rectNumber == 47 || rectNumber == 55 || rectNumber == 63) {
+            rectCoordX = (width*7)-xIn;
+        }
         return rectCoordX;
     }
 
     public int getRectCoordY(int rectNumber) {
-        int yNumber;
-        yNumber = rectNumber / 8;
-        rectCoordY = (yNumber*height)+yIn;
+        if(rectNumber == 0 || rectNumber == 1 || rectNumber == 2 || rectNumber == 3 || rectNumber == 4 || rectNumber == 5 || rectNumber == 6 || rectNumber == 7) {
+            rectCoordY = (height*7)-yIn;
+        }
+        else if(rectNumber == 8 || rectNumber == 9 || rectNumber == 10 || rectNumber == 11 || rectNumber == 12 || rectNumber == 13 || rectNumber == 14 || rectNumber == 15) {
+            rectCoordY = (height*6)-yIn;
+        }
+        else if(rectNumber == 16 || rectNumber == 17 || rectNumber == 18 || rectNumber == 19 || rectNumber == 20 || rectNumber == 21 || rectNumber == 22 || rectNumber == 23) {
+            rectCoordY = (height*5)-yIn;
+        }
+        else if(rectNumber == 24 || rectNumber == 25 || rectNumber == 26 || rectNumber == 27 || rectNumber == 28 || rectNumber == 29 || rectNumber == 30 || rectNumber == 31) {
+            rectCoordY = (height*4)-yIn;
+        }
+        else if(rectNumber == 32 || rectNumber == 33 || rectNumber == 34 || rectNumber == 35 || rectNumber == 36 || rectNumber == 37 || rectNumber == 38 || rectNumber == 39) {
+            rectCoordY = (height*3)-yIn;
+        }
+        else if(rectNumber == 40 || rectNumber == 41 || rectNumber == 42 || rectNumber == 43 || rectNumber == 44 || rectNumber == 45 || rectNumber == 46 || rectNumber == 47) {
+            rectCoordY = (height*2)-yIn;
+        }
+        else if(rectNumber == 48 || rectNumber == 49 || rectNumber == 50 || rectNumber == 51 || rectNumber == 52 || rectNumber == 53 || rectNumber == 54 || rectNumber == 55) {
+            rectCoordY = (height*1)-yIn;
+        }
+        else if(rectNumber == 56 || rectNumber == 57 || rectNumber == 58 || rectNumber == 59 || rectNumber == 60 || rectNumber == 61 || rectNumber == 62 || rectNumber == 63) {
+            rectCoordY = (height*0)-yIn;
+        }
         return rectCoordY;
     }
     public static void main(String[] args) {
         getRect getRect = new getRect();
         System.out.println(getRect.getRectNumber(400, 400));
-        System.out.println(getRect.getRectCoordY(4));
+        System.out.println(getRect.getRectCoordX(56));
+        System.out.println(getRect.getRectCoordY(56));
     }
 
 }
