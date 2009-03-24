252d65dd59d63d6066dc60c9ec9a33f89e72b6d4
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-24 10:13:28 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@189 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/chess/gui/Mainwindow.java
+++ src/no/hist/aitel/chess/gui/Mainwindow.java
@@ -6,15 +6,11 @@ package no.hist.aitel.chess.gui;
  */
 
 import java.awt.BorderLayout;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -22,12 +18,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
 import no.hist.aitel.chess.board.Board;
 import no.hist.aitel.chess.board.IllegalTurnException;
 import no.hist.aitel.chess.position.IllegalPositionException;
@@ -35,7 +27,7 @@ import no.hist.aitel.chess.piece.*;
 import static no.hist.aitel.chess.gui.guiConstants.*;
 
 
-class Mainwindow extends JFrame implements MouseListener, MouseMotionListener {
+public class Mainwindow extends JFrame implements MouseListener, MouseMotionListener {
     private boolean canDrag = true;
     private Chessboard boardGui = new Chessboard();
     private Board board = new Board();
@@ -359,89 +351,3 @@ class Mainwindow extends JFrame implements MouseListener, MouseMotionListener {
     }
 }
 
-class testGui implements ActionListener, ItemListener {
-        private static Mainwindow mainWindow = new Mainwindow("chess");
-    
-    private static void createAndShowGui() {
-        JFrame frame = mainWindow;
-        testGui testGui = new testGui();
-        frame.setJMenuBar(testGui.addJMenu());
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        frame.pack();
-    }
-
-
-    private JMenuBar addJMenu() {
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File"), submenu = new JMenu("Set player names");
-        menu.setMnemonic(KeyEvent.VK_A);
-        menu.getAccessibleContext().setAccessibleDescription("Test");
-        menuBar.add(menu);
-
-        JMenuItem menuItem = new JMenuItem("Save game", KeyEvent.VK_T);
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F2, ActionEvent.ALT_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
-        menu.add(menuItem);
-
-        menu.addSeparator();
-        submenu.setMnemonic(KeyEvent.VK_S);
-        menuItem = new JMenuItem("Player 1");
-        menuItem.addActionListener(this);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
-        submenu.add(menuItem);
-
-        menuItem = new JMenuItem("Player 2");        
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK));
-        menuItem.addActionListener(this);
-        submenu.add(menuItem);
-
-        menu.add(submenu);
-        
-        return menuBar;
-
-    }
-
-    protected String getClassName(Object o) {
-        String classString = o.getClass().getName();
-        int dotIndex = classString.lastIndexOf(".");
-        return classString.substring(dotIndex+1);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        JMenuItem source = (JMenuItem)(e.getSource());
-        String s = "Action event detected."
-                + "\n"
-                + "    Event source: " + source.getText()
-                + " (an instance of " + getClassName(source) + ")";
-        System.out.println(s);
-        if(e.getActionCommand().equals("Player 1")) {
-            String newP1name = JOptionPane.showInputDialog(null, "Player 1 name:");
-            if(newP1name != "") {
-                mainWindow.setP1name(newP1name);
-            }
-        }
-        else if(e.getActionCommand().equals("Player 2")) {
-            String newP2name = JOptionPane.showInputDialog(null, "Player 2 name:");
-            if(newP2name != "") {
-                mainWindow.setP2name(newP2name);
-            }
-        }
-    }
-
-    public void itemStateChanged(ItemEvent e) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGui();
-            }
-        });
-    }
-}
-     
---
+++ src/no/hist/aitel/chess/gui/gui.java
@@ -0,0 +1,106 @@
+
+
+package no.hist.aitel.chess.gui;
+
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+
+/**
+ *
+ * @author Vegard
+ */
+public class gui implements ActionListener, ItemListener {
+    private static Mainwindow mainWindow = new Mainwindow("chess");
+
+    private static void createAndShowGui() {
+        JFrame frame = mainWindow;
+        gui gui = new gui();
+        frame.setJMenuBar(gui.addJMenu());
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        frame.pack();
+    }
+
+
+    private JMenuBar addJMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File"), submenu = new JMenu("Set player names");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription("Test");
+        menuBar.add(menu);
+
+        JMenuItem menuItem = new JMenuItem("Save game", KeyEvent.VK_T);
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F2, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+        menu.add(menuItem);
+
+        menu.addSeparator();
+        submenu.setMnemonic(KeyEvent.VK_S);
+        menuItem = new JMenuItem("Player 1");
+        menuItem.addActionListener(this);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Player 2");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        menuItem.addActionListener(this);
+        submenu.add(menuItem);
+
+        menu.add(submenu);
+
+        return menuBar;
+
+    }
+
+    protected String getClassName(Object o) {
+        String classString = o.getClass().getName();
+        int dotIndex = classString.lastIndexOf(".");
+        return classString.substring(dotIndex+1);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JMenuItem source = (JMenuItem)(e.getSource());
+        String s = "Action event detected."
+                + "\n"
+                + "    Event source: " + source.getText()
+                + " (an instance of " + getClassName(source) + ")";
+        System.out.println(s);
+        if(e.getActionCommand().equals("Player 1")) {
+            String newP1name = JOptionPane.showInputDialog(null, "Player 1 name:");
+            if(newP1name != "") {
+                mainWindow.setP1name(newP1name);
+            }
+        }
+        else if(e.getActionCommand().equals("Player 2")) {
+            String newP2name = JOptionPane.showInputDialog(null, "Player 2 name:");
+            if(newP2name != "") {
+                mainWindow.setP2name(newP2name);
+            }
+        }
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGui();
+            }
+        });
+    }
+}
