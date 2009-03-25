982d0a57486c3d461dcb328af8bdaeac0313ed13
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-03-25 15:12:42 +0000

    git-svn-id: file:///home/martin/svn/repositories/chess@219 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/chess/gui/Main.java
+++ src/no/hist/aitel/chess/gui/Main.java
@@ -3,7 +3,6 @@
 package no.hist.aitel.chess.gui;
 
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
