264addb980a0a3130d9cdc6f2542d64e2739e739
vegard <vegard@a023ea85-94b2-48ad-8667-9024dedc06d2>
2009-04-28 21:34:08 +0000

    små endringa
    git-svn-id: file:///home/martin/svn/repositories/chess@381 a023ea85-94b2-48ad-8667-9024dedc06d2

--- src/no/hist/aitel/chess/gui/guiConstants.java
+++ src/no/hist/aitel/chess/gui/guiConstants.java
@@ -13,13 +13,33 @@ public final class guiConstants {
 
     private guiConstants() {
     }
-
+    /**
+     * The constant width determinates the width of the chessboard squares and the pieces
+     */
     public static final int width = 60;
+    /**
+     * The constant height determinates the height of the chessboard squares and the pieces
+     */
     public static final int height = 60;
+    /**
+     * The constant xIn determinates the amount of pixels into the screen, seen from the left, the chessboard starts
+     */
     public static final int xIn = 17;
+    /**
+     * The constant yIn determinates the amount of pixels into the screen, seen from the top, the chessboard starts
+     */
     public static final int yIn = 155;
+    /**
+     * The constant y_calibrate determinates the calibration of the pieces, seen from the top, into the chessboard
+     */
     public static final int y_calibrate = 61;
+    /**
+     * The constant boardSize determinates how many squares there are in the board
+     */
     public static final int boardSize = 64;
+    /**
+     * The constant zero determinates the value 0
+     */
     public static final int zero = 0;
     
     public static final Color background = new Color (0xD8D8BF);
